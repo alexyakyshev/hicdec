19ebbbf4b52e639f6941786db49679dcef9a9f5a
alexyakyshev <r1kanat@yandex.ru>
2024-04-12 00:16:11 +0300

    add __init__

---
+++ models/features/__init__.py
@@ -0,0 +1,13 @@
+from .base import Feature
+from .compartments import CompartmentFeature
+from .fountains import FountainsFeature
+from .insulation import InsulationFeature
+from .stripes import StripesFeature
+
+__all__ = [
+    'Feature',
+    'CompartmentFeature',
+    'FountainsFeature',
+    'InsulationFeature',
+    'StripesFeature'
+    ]
